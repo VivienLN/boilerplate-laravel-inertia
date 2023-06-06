20102b58460834e3f0295bd4db08766ccf7aee12
VivienLN <vivienleneez@gmail.com>
2023-06-06 16:10:06 +0200

    update config for docker

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,31 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
+import vue from '@vitejs/plugin-vue';
 
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
+            input: 'resources/js/app.js',
             refresh: true,
         }),
+        vue({
+            template: {
+                transformAssetUrls: {
+                    base: null,
+                    includeAbsolute: false,
+                },
+            },
+        }),
     ],
+    // Added here to work with docker...
+    server: {
+      host: true,
+      port: 5173,
+      watch: {
+        usePolling: true
+      },
+      hmr: {
+        host: 'localhost',
+      }
+    }
 });
